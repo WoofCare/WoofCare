894c6d9cdc31face1b650643c221ca0a985bbea9
Daniel-Abrams <danlab1104@gmail.com>
2025-04-01 12:16:30 -0700

    Added basic sreport submission backend code

--- Source Code/app/lib/ui/pages/report/reporting.dart
+++ Source Code/app/lib/ui/pages/report/reporting.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-//import 'package:woofcare/config/constants.dart';
+import 'package:woofcare/config/constants.dart';
 import 'package:woofcare/ui/widgets/custom_button.dart';
 import 'package:woofcare/ui/widgets/custom_textfield.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ReportingPage extends StatefulWidget {
   const ReportingPage({super.key});
@@ -16,6 +17,24 @@ class _ReportingPageState extends State<ReportingPage> {
   final _locationDescriptionController = TextEditingController();
   final _extraNotesController = TextEditingController();
 
+  void submitReport() async {
+    CollectionReference reports =
+        FirebaseFirestore.instance.collection('Reports');
+
+    Map<String, dynamic> newReportData = {
+      'userID': profile.id,
+      'title': _reportTitleController.text,
+      'description': _dogDescriptionController.text,
+      'location_description': _locationDescriptionController.text,
+    };
+
+    try {
+      await reports.add(newReportData);
+      print("Report successfully submitted!");
+    } catch (e) {
+      print("Failed to submit report.");
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -31,109 +50,108 @@ class _ReportingPageState extends State<ReportingPage> {
             borderRadius: BorderRadius.circular(24),
             boxShadow: const [
               BoxShadow(
-                color:Color(0x1A000000),
-                offset: Offset(4, 4),
-                blurRadius: 7.0,
-                spreadRadius: 2.0
-              ),
+                  color: Color(0x1A000000),
+                  offset: Offset(4, 4),
+                  blurRadius: 7.0,
+                  spreadRadius: 2.0),
               BoxShadow(
-                color:Color.fromARGB(255, 235, 165, 99),
+                color: Color.fromARGB(255, 235, 165, 99),
                 offset: Offset(-1, -1),
                 blurRadius: 5.0,
                 spreadRadius: 0.0,
               ),
             ],
           ),
-      
+
           // Stack to allow for better precision in positioning widgets
-          child: Stack(
-            children: [
-              Column(   // Column to hold the text fields
-                children: [
-                  const SizedBox(height: 35),
-              
-                  // Report Title text field
-                  CustomTextField(
-                    controller: _reportTitleController, 
-                    hintText: 'Report Title'
-                  ),
-              
-                  const SizedBox(height: 20),
-              
-                  // Dog Description text field
-                  CustomTextField(
-                    controller: _dogDescriptionController, 
-                    hintText: 'Identifying features of dog? (i.e Patterns, injuries, gender, etc.)',
-                    top: 25,
-                    bottom: 25,
-                  ),
-              
-                  const SizedBox(height: 20),
-              
-                  // Location Description text field
-                  CustomTextField(
-                    controller: _locationDescriptionController, 
-                    hintText: 'Identifying features of location? (i.e Landmarks, dangers, time etc.)',
-                    top: 25,
-                    bottom: 25,
-                  ),
-              
-                  const SizedBox(height: 20),
-              
-                  // Additional Notes text field
-                  CustomTextField(
-                    controller: _extraNotesController, 
-                    hintText: 'Additional Notes?',
-                  ),              
-                ],
-              ),
-      
-              // Add Photos button
-              Positioned(
-                right: 262,
-                bottom: 350,
-                child: CustomButton(
-                  text: 'Add Photos',
-                  fontSize: 10,
-                  edgeInstetAll: 10,
-                  onTap: () => print('Add Photos button pressed'), //TODO: Implement photo upload
+          child: Stack(children: [
+            Column(
+              // Column to hold the text fields
+              children: [
+                const SizedBox(height: 35),
+
+                // Report Title text field
+                CustomTextField(
+                    controller: _reportTitleController,
+                    hintText: 'Report Title'),
+
+                const SizedBox(height: 20),
+
+                // Dog Description text field
+                CustomTextField(
+                  controller: _dogDescriptionController,
+                  hintText:
+                      'Identifying features of dog? (i.e Patterns, injuries, gender, etc.)',
+                  top: 25,
+                  bottom: 25,
                 ),
+
+                const SizedBox(height: 20),
+
+                // Location Description text field
+                CustomTextField(
+                  controller: _locationDescriptionController,
+                  hintText:
+                      'Identifying features of location? (i.e Landmarks, dangers, time etc.)',
+                  top: 25,
+                  bottom: 25,
+                ),
+
+                const SizedBox(height: 20),
+
+                // Additional Notes text field
+                CustomTextField(
+                  controller: _extraNotesController,
+                  hintText: 'Additional Notes?',
+                ),
+              ],
+            ),
+
+            // Add Photos button
+            Positioned(
+              right: 262,
+              bottom: 350,
+              child: CustomButton(
+                text: 'Add Photos',
+                fontSize: 10,
+                edgeInstetAll: 10,
+                onTap: () => print(
+                    'Add Photos button pressed'), //TODO: Implement photo upload
               ),
-      
-              // Photo container (and camera placeholder)
-              Positioned(
-                left: 28, 
+            ),
+
+            // Photo container (and camera placeholder)
+            Positioned(
+                left: 28,
                 bottom: 140,
                 child: Container(
-                  width: 330,
-                  height: 200,
-                  decoration: BoxDecoration(
-                    color: const Color(0xFFA66E38).withOpacity(0.3),
-                    borderRadius: BorderRadius.circular(8.0),
-                  ),
-                  child: const Image(
-                    image: AssetImage('assets/images/placeholders/camera.png'),
-                    fit: BoxFit.scaleDown,
-                  )
-                )
-              ),
-              
-              // Submit button
-              Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  CustomButton(
-                    text: 'Submit', 
-                    fontSize: 20,
-                    onTap: () => print('Submit button pressed'), //TODO: Implement submit button
-                    edgeSymmetricHorizontal: 80,
-                    edgeSymmetricVertical: 40,
-                    edgeInstetAll: 20,
-                  ),
-                ],
-              )
-            ]
-          ),          
+                    width: 330,
+                    height: 200,
+                    decoration: BoxDecoration(
+                      color: const Color(0xFFA66E38).withOpacity(0.3),
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                    child: const Image(
+                      image:
+                          AssetImage('assets/images/placeholders/camera.png'),
+                      fit: BoxFit.scaleDown,
+                    ))),
+
+            // Submit button
+            Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                CustomButton(
+                  text: 'Submit',
+                  fontSize: 20,
+                  onTap: () => submitReport(),
+                  edgeSymmetricHorizontal: 80,
+                  edgeSymmetricVertical: 40,
+                  edgeInstetAll: 20,
+                ),
+              ],
+            )
+          ]),
         ),
       ),
     );
