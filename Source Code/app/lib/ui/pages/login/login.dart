e25160e7d2b4d5956638f060ff20bb72a93712b3
jesus-aguilar-pro <jgaguil5@asu.edu>
2025-06-13 11:21:41 -0700

    Removed scroll behavior and fixed padding to prevent content overflow

--- Source Code/app/lib/ui/pages/login/login.dart
+++ Source Code/app/lib/ui/pages/login/login.dart
@@ -57,8 +57,8 @@ class _LogInPageState extends State<LogInPage> {
               padding: const EdgeInsets.only(
                 left: 25.0,
                 right: 25.0,
-                top: 110.0,
-                bottom: 100.0,
+                top: 90.0,
+                bottom: 90.0,
               ),
               child: Container(
                 // height: 30.0.sh,
@@ -82,167 +82,166 @@ class _LogInPageState extends State<LogInPage> {
                   ],
                 ), // Background color of the container
                 child: Padding(
-                  padding: const EdgeInsets.only(top: 50.0),
-                  child: SingleChildScrollView(
-                    child: Center(
-                      // Center the contents
-                      child: Column(
-                        children: [
-                          //Welcome Back Message
-                          const Text(
-                            "Welcome Back to \n      WoofCare!", // There is probably a better way to do this XD
-                            style: TextStyle(
-                              color: Color(0xFF3F2917),
-                              fontSize: 30,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                              
-                          const SizedBox(height: 5),
-                              
-                          const Divider(
-                            color: Color(0xFF3F2917),
-                            thickness: 3,
-                            indent: 50,
-                            endIndent: 50,
-                          ),
-                              
-                          const SizedBox(height: 35),
-                              
-                          //Username Field
-                          CustomTextField(
-                            controller: _emailTextController,
-                            hintText: "Email",
-                            prefix: Icons.email,
-                          ),
-                              
-                          const SizedBox(height: 35),
-                              
-                          //Password Field
-                          CustomTextField(
-                            controller: _passwordTextController,
-                            hintText: "Password",
-                            obscureText: true,
-                            prefix: Icons.password,
-                            maxLines: 1,
-                          ),
-                              
-                          const SizedBox(height: 7),
-                              
-                          Padding(
-                            padding: const EdgeInsets.symmetric(
-                              horizontal: 30.0,
-                            ),
-                            child: Row(
-                              mainAxisAlignment:
-                                  MainAxisAlignment.spaceBetween,
+                  padding: const EdgeInsets.only(top: 40.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+
+                    children: [
+                      //Welcome Back Message
+                      const Text(
+                        textAlign: TextAlign.center,
+                        "Welcome Back to WoofCare!", // There is probably a better way to do this XD
+                        style: TextStyle(
+                          color: Color(0xFF3F2917),
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),  
+                        
+                      const SizedBox(height: 5),
+                          
+                      const Divider(
+                        color: Color(0xFF3F2917),
+                        thickness: 3,
+                        indent: 50,
+                        endIndent: 50,
+                      ),
+                          
+                      const SizedBox(height: 35),
+                          
+                      //Username Field
+                      CustomTextField(
+                        controller: _emailTextController,
+                        hintText: "Email",
+                        prefix: Icons.email,
+                      ),
+                          
+                      const SizedBox(height: 35),
+                          
+                      //Password Field
+                      CustomTextField(
+                        controller: _passwordTextController,
+                        hintText: "Password",
+                        obscureText: true,
+                        prefix: Icons.password,
+                        maxLines: 1,
+                      ),
+                          
+                      const SizedBox(height: 7),
+                          
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 30.0,
+                        ),
+                        child: Row(
+                          mainAxisAlignment:
+                              MainAxisAlignment.spaceBetween,
+                          children: [
+                            //Remember Me Text & Checkbox
+                            Row(
                               children: [
-                                //Remember Me Text & Checkbox
-                                Row(
-                                  children: [
-                                    const Text(
-                                      "Remember Me",
-                                      style: TextStyle(
-                                        color: Color(0xFF3F2917),
-                                        fontSize: 12,
-                                      ),
-                                    ),
-                                    Checkbox(
-                                      value: rememberMe,
-                                      side: const BorderSide(),
-                                      focusColor: Colors.green,
-                                      onChanged: (bool? value) {
-                                        setState(() {
-                                          rememberMe = !rememberMe;
-                                        });
-                                      },
-                                    ),
-                                  ],
-                                ),
-                              
-                                // Forgot Password Button
-                                RichText(
-                                  text: TextSpan(
-                                    text: "Forgot Password?",
-                                    style: theme.textTheme.bodyMedium!
-                                        .copyWith(
-                                          color: const Color(0xFFA66E38),
-                                        ),
-                                    recognizer:
-                                        TapGestureRecognizer()
-                                          ..onTap = forgotPassword,
+                                const Text(
+                                  "Remember Me",
+                                  style: TextStyle(
+                                    color: Color(0xFF3F2917),
+                                    fontSize: 12,
                                   ),
                                 ),
-                              ],
-                            ),
-                          ),
-
-                          // Error Message    
-                          Padding(
-                            padding: const EdgeInsets.all(8),
-                            child: Text(
-                              _errorTextController.text,
-                              style: const TextStyle(
-                                color: Colors.black,
-                                fontSize: 20,
-                              ),
-                            ),
-                          ),
-                              
-                          const SizedBox(height: 35),
-                              
-                          //Log In Button
-                          CustomButton(
-                            text: "Log In",
-                            onTap:
-                                () => Auth.login(
-                                  context: context,
-                                  email: _emailTextController.text,
-                                  password: _passwordTextController.text,
-                                  error: (e) {
+                                Checkbox(
+                                  value: rememberMe,
+                                  side: const BorderSide(),
+                                  focusColor: Colors.green,
+                                  onChanged: (bool? value) {
                                     setState(() {
-                                      errorMessage = e.toString();
-                                      _errorTextController.text =
-                                          errorMessage ?? '';
+                                      rememberMe = !rememberMe;
                                     });
                                   },
                                 ),
-                          ),
-                              
-                          const SizedBox(height: 15),
-                              
-                          //First Time User? Sign Up Button
-                          Padding(
-                            padding: const EdgeInsets.only(bottom: 50.0),
-                            child: RichText(
+                              ],
+                            ),
+                          
+                            // Forgot Password Button
+                            RichText(
                               text: TextSpan(
-                                text: "Don't have an account? ",
-                                style: const TextStyle(
-                                  fontFamily: "ABeeZee",
-                                  color: Color(0xFF3F2917),
-                                  fontSize: 12,
-                                ),
-                                children: <TextSpan>[
-                                  TextSpan(
-                                    text: "Sign Up",
-                                    style: theme.textTheme.bodyMedium!.copyWith(
+                                text: "Forgot Password?",
+                                style: theme.textTheme.bodyMedium!
+                                    .copyWith(
                                       color: const Color(0xFFA66E38),
                                     ),
-                                    recognizer:
-                                        TapGestureRecognizer()
-                                          ..onTap =
-                                              () => Navigator.pushNamed(
-                                                context,
-                                                "/signup",
-                                              ),
-                                  ),
-                                ],
+                                recognizer:
+                                    TapGestureRecognizer()
+                                      ..onTap = forgotPassword,
                               ),
                             ),
-                          ),
-                        ],
+                          ],
+                        ),
                       ),
-                    ),
+                                    
+                      // Error Message    
+                      Padding(
+                        padding: const EdgeInsets.all(8),
+                        child: Text(
+                          _errorTextController.text,
+                          style: const TextStyle(
+                            color: Colors.black,
+                            fontSize: 20,
+                          ),
+                        ),
+                      ),
+                          
+                      const SizedBox(height: 35),
+                          
+                      //Log In Button
+                      CustomButton(
+                        text: "Log In",
+                        margin: 30,
+                        onTap:
+                            () => Auth.login(
+                              context: context,
+                              email: _emailTextController.text,
+                              password: _passwordTextController.text,
+                              error: (e) {
+                                setState(() {
+                                  errorMessage = e.toString();
+                                  _errorTextController.text =
+                                      errorMessage ?? '';
+                                });
+                              },
+                            ),
+                      ),
+                          
+                      const SizedBox(height: 15),
+                          
+                      //First Time User? Sign Up Button
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 50.0),
+                        child: RichText(
+                          text: TextSpan(
+                            text: "Don't have an account? ",
+                            style: const TextStyle(
+                              fontFamily: "ABeeZee",
+                              color: Color(0xFF3F2917),
+                              fontSize: 12,
+                            ),
+                            children: <TextSpan>[
+                              TextSpan(
+                                text: "Sign Up",
+                                style: theme.textTheme.bodyMedium!.copyWith(
+                                  color: const Color(0xFFA66E38),
+                                ),
+                                recognizer:
+                                    TapGestureRecognizer()
+                                      ..onTap =
+                                          () => Navigator.pushNamed(
+                                            context,
+                                            "/signup",
+                                          ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
               ),
