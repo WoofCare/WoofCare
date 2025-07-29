62d7b79a06764171ebc3f164f67cf7361d27115d
jesus-aguilar-pro <jgaguil5@asu.edu>
2025-07-29 12:46:11 -0700

    Improved post widget UI (including profile pic icon, timestamp, user, text)

--- Source Code/app/lib/ui/pages/posts/posts.dart
+++ Source Code/app/lib/ui/pages/posts/posts.dart
@@ -84,9 +84,21 @@ class _SocialMediaFeedState extends State<SocialMediaFeed> {
               child: 
               Column(
                 children: [
-                  Post(), 
-                  Post(), 
-                  Post(),
+                  Post(
+                    message: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+                    user: "Jesus Aguilar",
+                    time: "07/29/2025 - 10:52 AM",
+                  ), 
+                  Post(
+                    message: "Hello",
+                    user: "Jesus Aguilar",
+                    time: "07/29/2025 - 10:52 AM",
+                  ), 
+                  Post(
+                    message: "Hello",
+                    user: "Jesus Aguilar",
+                    time: "07/29/2025 - 10:52 AM",
+                  ),
                 ] 
               ),
             ),
--- Source Code/app/lib/ui/widgets/post_widget.dart
+++ Source Code/app/lib/ui/widgets/post_widget.dart
@@ -1,40 +1,111 @@
 import 'package:flutter/material.dart';
 import 'package:woofcare/config/colors.dart';
 
-class Post extends StatefulWidget {
-  const Post({super.key});
+class Post extends StatelessWidget {
+  final String message;
+  final String user;
+  final String time;
+  
+  const Post({
+    super.key,
+    required this.message,
+    required this.user,
+    required this.time,
+  });
 
-  @override
-  State<Post> createState() => _PostState();
-}
-
-class _PostState extends State<Post> {
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
-      child: ConstrainedBox(
-        constraints: BoxConstraints.expand(height: 500),
-        child: Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(14.0),
-            color: WoofCareColors.postBackground,
-          ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Expanded(child: Icon(Icons.insert_photo)),
-
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      child: Container(
+        // constraints: BoxConstraints(
+        //   maxWidth: 300
+        // ),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(14.0),
+          color: WoofCareColors.postBackground,
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                spacing: 10.0,
                 children: [
-                  IconButton(onPressed: () {}, icon: Icon(Icons.thumb_up), color: WoofCareColors.inputBackground,),
-                  IconButton(onPressed: () {}, icon: Icon(Icons.comment), color: WoofCareColors.inputBackground,),
-                  IconButton(onPressed: () {}, icon: Icon(Icons.share), color: WoofCareColors.inputBackground,),
+                  // Expanded(child: Icon(Icons.insert_photo)),
+                  CircleAvatar(
+                    backgroundColor: Color(0xFFCAB096),
+                    child: Icon(
+                      Icons.person,
+                      color: WoofCareColors.primaryTextAndIcons,
+                    ),
+                  ),
+                
+                  Flexible(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      spacing: 5.0,
+                      children: [
+                        Row(
+                          spacing: 5.0,
+                          children: [
+                            Text(
+                              user, 
+                              style: TextStyle(
+                                color: WoofCareColors.primaryTextAndIcons
+                              ),
+                            ),
+                            Flexible(
+                              child: Text(
+                                time, 
+                                style: TextStyle(
+                                  color: WoofCareColors.primaryTextAndIcons.withValues(alpha: 0.5),
+                                ),
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                            )
+                          ],
+                        ),
+                      
+                        Text(
+                          message,
+                          style: TextStyle(
+                            color: WoofCareColors.primaryTextAndIcons
+                          ),
+                          // maxLines: 1,
+                          // softWrap: false,
+                          // overflow: TextOverflow.fade,
+                        ),
+                      ],
+                    ),
+                  ),
                 ],
               ),
-            ],
-          ),
+            ),
+      
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.thumb_up),
+                  color: WoofCareColors.inputBackground,
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.comment),
+                  color: WoofCareColors.inputBackground,
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.share),
+                  color: WoofCareColors.inputBackground,
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
