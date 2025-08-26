caea2285b5848caaf5cabcf1f44d931e0478844d
JBarroqu <johnnessambarroquillo@gmail.com>
2025-08-26 12:48:08 -0700

    Fixed Header
    fix header information + added some colors for sharing

--- Source Code/app/lib/ui/pages/profile/profile.dart
+++ Source Code/app/lib/ui/pages/profile/profile.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-
+import 'package:woofcare/ui/pages/home/home.dart';
+import 'package:woofcare/ui/widgets/custom_button.dart';
 import '/config/colors.dart';
 import '/config/constants.dart';
 import '/ui/widgets/custom_button.dart';
@@ -46,212 +47,239 @@ class _ProfilePageState extends State<ProfilePage> {
           },
           child: Column(
             children: [
-              Container(
-                // Container for the first background image
-                decoration: const BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage(
-                      "assets/images/patterns/BigPawPattern.png",
+               Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+
+                const SizedBox(width: 20), // Spacer on the left
+            
+                // Profile Picture
+                
+                GestureDetector(
+                  onTap: () {}, //TODO: Add photo function
+                  child: Container(
+                    width: 150,
+                    height: 150,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      border: Border.all(
+                        color: WoofCareColors.primaryTextAndIcons, // Stroke color
+                        width: 3.0, // Stroke width
+                      ),
                     ),
-                    alignment: Alignment.topLeft,
+                    child: const CircleAvatar(
+                      // CircleAvatar for the profile picture
+                      radius: 75,
+                      backgroundColor: Colors.white,
+                      child: Icon(
+                        color: WoofCareColors.primaryTextAndIcons,
+                        Icons.person,
+                        size: 100,
+                      ),
+                    )
+                  )
                   ),
+            
+                //spacer between picture and text
+                const SizedBox(
+                  width: 5,
                 ),
-              ),
-              Container(
-                // Container for the second background image
-                decoration: const BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage(
-                      "assets/images/patterns/SmallPawPattern.png",
-                    ),
-                    alignment: Alignment.bottomRight,
-                  ),
-                ),
-              ),
 
-              GestureDetector(
-                onTap: () {}, //TODO: Add photo function
-                child: const CircleAvatar(
-                  // CircleAvatar for the profile picture
-                  radius: 100,
-                  backgroundColor: Colors.white,
-                  child: Icon(
-                    color: WoofCareColors.primaryTextAndIcons,
-                    Icons.person,
-                    size: 150,
-                  ),
-                ),
-                // ),
-              ),
-
-              SizedBox(height: 20),
-
-              //Profile Information
-              Expanded(
-                flex: -2,
-                child: SingleChildScrollView(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Center(
-                    child: Column(
-                      mainAxisSize:
-                          MainAxisSize
+                //profile information 
+                Expanded(
+                    child: SafeArea(
+                      child: SingleChildScrollView(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Center(
+                        child: Column(
+                          mainAxisSize: MainAxisSize
                               .min, // This will minimize the space needed
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        // Username
-                        SizedBox(
-                          height: 30,
-                          width: 300,
-                          child: Text.rich(
-                            TextSpan(
-                              text: profile.name,
-                              style: const TextStyle(
-                                fontFamily: "Abeezee",
-                                fontWeight: FontWeight.bold,
-                                fontSize: 24,
-                                color: WoofCareColors.primaryTextAndIcons,
-                              ),
-                              children: const [
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            // Username
+                            SizedBox(
+                              height: 30,
+                              width: 300,
+                              child: Text.rich(
                                 TextSpan(
-                                  //TODO: connect username to database
-                                  text: " @username",
-                                  style: TextStyle(
-                                    fontSize: 12,
-                                    color: Color(0xFFA66E38),
+                                  text: profile.name,
+                                  style: const TextStyle(
+                                    fontFamily: "Abeezee",
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 24,
+                                    color: WoofCareColors.primaryTextAndIcons,
                                   ),
+                                  children: const [
+                                    TextSpan(
+                                      //TODO: connect username to database
+                                      text: " @username",
+                                      style: TextStyle(
+                                        fontSize: 12,
+                                        color: Color(0xFFA66E38),
+                                      ),
+                                    ),
+                                  ],
                                 ),
-                              ],
+                              ),
                             ),
-                          ),
-                        ),
-
-                        // Role
-                        SizedBox(
-                          width: 300,
-                          child: Text(
-                            profile.role,
-                            textAlign: TextAlign.left,
-                            style: const TextStyle(
-                              fontFamily: "Abeezee",
-                              fontWeight: FontWeight.w700,
-                              fontSize: 16,
-                              color: WoofCareColors.primaryTextAndIcons,
-                            ),
-                          ),
-                        ),
-
-                        const SizedBox(height: 5),
-
-                        //location
-                        const SizedBox(
-                          width: 300,
-                          child: Row(
-                            children: [
-                              Icon(
-                                Icons.location_on_outlined,
-                                size: 24,
-                                color: Color(0xFF805832),
-                              ),
-                              SizedBox(width: 3),
-                              Text(
-                                //TODO: connect location to database + allow privacy
-                                "Location",
-                                textAlign: TextAlign.left,
-                                style: TextStyle(
-                                  fontFamily: "Abeezee",
-                                  fontSize: 14,
-                                  color: Color(0xFF805832),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-
-                        const SizedBox(height: 5),
-
-                        //phone number
-                        const SizedBox(
-                          width: 300,
-                          child: Row(
-                            children: [
-                              Icon(
-                                Icons.phone,
-                                size: 24,
-                                color: Color(0xFF805832),
-                              ),
-                              SizedBox(width: 3),
-                              Text(
-                                //TODO: connect phone number to database + allow privacy
-                                "(123)456-7890",
-                                textAlign: TextAlign.left,
-                                style: TextStyle(
-                                  fontFamily: "Abeezee",
-                                  fontSize: 14,
-                                  color: Color(0xFF805832),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-
-                        const SizedBox(height: 3),
-
-                        //email
-                        SizedBox(
-                          width: 300,
-                          child: Row(
-                            children: [
-                              const Icon(
-                                Icons.mail_outline,
-                                size: 24,
-                                color: Color(0xFF805832),
-                              ),
-                              const SizedBox(width: 3),
-                              Text(
-                                profile.email,
+            
+                            // Role
+                            SizedBox(
+                              width: 300,
+                              child: Text(
+                                profile.role,
                                 textAlign: TextAlign.left,
                                 style: const TextStyle(
                                   fontFamily: "Abeezee",
-                                  fontSize: 14,
-                                  color: Color(0xFF805832),
+                                  fontWeight: FontWeight.w700,
+                                  fontSize: 16,
+                                  color: WoofCareColors.primaryTextAndIcons,
                                 ),
                               ),
-                            ],
-                          ),
+                            ),
+            
+                            const SizedBox(
+                              height: 5,
+                            ),
+            
+                            //location
+                            const SizedBox(
+                              width: 300,
+                              child: Row(
+                                children: [
+                                  Icon(
+                                    Icons.location_on_outlined,
+                                    size: 24,
+                                    color: Color(0xFF805832),
+                                  ),
+                                  SizedBox(width: 3),
+                                  Text(
+                                    //TODO: connect location to database + allow privacy
+                                    "Location",
+                                    textAlign: TextAlign.left,
+                                    style: TextStyle(
+                                      fontFamily: "Abeezee",
+                                      fontSize: 14,
+                                      color: Color(0xFF805832),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+            
+                            const SizedBox(
+                              height: 5,
+                            ),
+            
+                            //phone number
+                            const SizedBox(
+                              width: 300,
+                              child: Row(
+                                children: [
+                                  Icon(
+                                    Icons.phone,
+                                    size: 24,
+                                    color: Color(0xFF805832),
+                                  ),
+                                  SizedBox(width: 3),
+                                  Text(
+                                    //TODO: connect phone number to database + allow privacy
+                                    "(123)456-7890",
+                                    textAlign: TextAlign.left,
+                                    style: TextStyle(
+                                      fontFamily: "Abeezee",
+                                      fontSize: 14,
+                                      color: Color(0xFF805832),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+            
+                            const SizedBox(
+                              height: 3,
+                            ),
+            
+                            //email
+                            SizedBox(
+                              width: 300,
+                              child: Row(
+                                children: [
+                                  const Icon(
+                                    Icons.mail_outline,
+                                    size: 24,
+                                    color: Color(0xFF805832),
+                                  ),
+                                  const SizedBox(width: 3),
+                                  Text(
+                                    profile.email,
+                                    textAlign: TextAlign.left,
+                                    style: const TextStyle(
+                                      fontFamily: "Abeezee",
+                                      fontSize: 14,
+                                      color: Color(0xFF805832),
+                                    )
+                                  )
+                                ],
+                              ),
+                            ),
+                          ],
                         ),
-                      ],
-                    ),
-                  ),
+                      ),
+                      )
+                    )
                 ),
+                const SizedBox(
+                  width: 20,
+                ), // Spacer on the right
+              ],
               ),
-
+               
+              // Spacer
               const SizedBox(height: 20),
+
+            
               // Button Row
               //TODO: add functionality to buttons
               Row(
                 children: [
-                  CustomButton(
-                    height: 60,
-                    width: 200,
-                    color: const Color(0xFFCAB096),
-                    fontColor: WoofCareColors.primaryTextAndIcons,
-                    fontSize: 14,
-                    borderRadius: 16,
-                    text: "Message",
-                    onTap: () {
-                      Navigator.pushNamed(context, "/home");
-                    },
-                  ),
-                  CustomButton(
-                    height: 60,
-                    width: 140,
-                    borderRadius: 16,
-                    text: "Follow",
-                    fontSize: 14,
-                    onTap: () {
-                      Navigator.pushNamed(context, "/home");
-                    },
+                  Padding(
+                    padding: const EdgeInsets.only(left: 10, right:0),
+                    child: CustomButton(
+                        height: 60,
+                        width: 200,
+                        color: WoofCareColors.backgroundElementColor,
+                        fontColor: WoofCareColors.primaryTextAndIcons,
+                        fontSize: 14,
+                        borderRadius: 16,
+                        text: "Message",
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => const HomePage()),
+                          );
+                        },
+                      ),
+                    ),
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.only(right: 10, left: 0),
+                      child: CustomButton(
+                        height: 60,
+                        color: WoofCareColors.buttonColor,
+                        borderRadius: 16,
+                        text: "Follow",
+                        fontSize: 14,
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => const HomePage()),
+                          );
+                        },
+                      ),
+                    ),
                   ),
                 ],
               ),
